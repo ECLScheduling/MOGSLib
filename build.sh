82d7654dd87f7b0fb5fc9b6004366dbfe1103467
Alexandre de Limas Santana <alexandre.limas.santana@gmail.com>
2017-09-13 15:12:17 -0300

    Modified the structure so that the library can be included direcly into the source code of Charm

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rts_name_charm="charm";
+definitions_file="include/system/definitions.h"
+
+if [ "$1" = "$rts_name_charm" ]; then
+  echo "Defining constants for precompiler";
+  printf "#pragma once\n\n" > "$definitions_file";
+
+  printf "// This line states that the build is intended for the Charm environment.\n" >> "$definitions_file";
+  printf "#define RTS_IS_CHARM" >> "$definitions_file";
+else
+  echo "Deu ruim"
+fi;
--- include/system/definitions.h
+++ include/system/definitions.h
@@ -1,20 +1,4 @@
-#pragma once 
+#pragma once
 
-#include <system/static.h>
-
-namespace Definitions {
-
-constexpr auto charmRTS = "Charm";
-constexpr auto noRTS = "None";
-
-}
-
-#ifndef RTS
-#define RTS Definitions::noRTS
-#endif
-
-#if static_functions::const_string_equal(RTS, Definitions::charmRTS)
+// This line states that the build is intended for the Charm environment.
 #define RTS_IS_CHARM
-#elif static_functions::const_string_equal(RTS, Definitions::noRTS)
-#define RTS_IS_NONE
-#endif
