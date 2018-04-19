d51680ef9fba21e313b039cc1b956d12110c33f0
Alexandre de Limas Santana <alexandre.limas.santana@gmail.com>
2018-04-19 20:19:07 -0300

    Made the generate groups using a different type of sparse matrix in the same file. Next step is to test the function implementation.

--- include/algorithms/eagerMap/eagerMapAlgorithm.h
+++ include/algorithms/eagerMap/eagerMapAlgorithm.h
@@ -2,8 +2,30 @@
 
 #include <system/traits.h>
 #include <algorithms/utility.h>
+#include "structures/sparseMatrix.h"
 
 namespace EagerMap {
+
+using UInt = Traits<DefaultTypes>::UInt;
+
+/**
+ * @brief The struct that defines a sparse matrix tuned for the EagerMap algorithm.
+ *
+ * @type IndexType The type that represents a task id for which the communication is targeted.
+ * @type DataType The type that represents the communication quantity.
+ */
+template<typename IndexType, typename DataType>
+struct CommunicationSparseMatrix {
+  using MatrixCell = std::map<IndexType, DataType>;
+  using DataArray = std::vector<MatrixCell>;
+  
+  DataArray elements;
+
+  CommunicationSparseMatrix(DataArray::size_type size) : elements(DataArray(size)) {
+    for(decltype(size) i = 0; i < size; ++i)
+      elements[] = MatrixCell();
+  };
+};
 
 /**
  * @brief The struct that defines the generic EagerMap scheduler algorithms.
@@ -11,11 +33,40 @@ namespace EagerMap {
  * @type Load A type that serve as a load abstraction to the algorithms.
  * @type UInt A type that serve as an unsigned integer to the algorithms.
  */
-template<typename Load, typename UInt>
+template<typename Load>
 struct Algorithms {
-  typename CommMatrix = int
+  using IndexType = UInt;
+  using CommVal = UInt;
+  using CommMatrix = CommunicationSparseMatrix<IndexType,CommVal>;
+  using Group = std::vector<IndexType>;
 
-  void generateGroups(CommMatrix comm, UInt total_elements, UInt group_elements, bool *chosen);
+  /**
+   * @brief Generates a group of tasks based on communication affinity.
+   * @param comm The communication matrix.
+   * @param total_elements The total amount of elements (rows) in the matrix.
+   * @param group_elements The wanted size of the generated group.
+   * @param chosen a boolean array to mark the chosen elements for a group to avoid selecting the same element twice.
+   * @param previous_groups The array containing the groups of previous iterations.
+   */
+  static Group* generate_groups(CommMatrix comm, UInt total_elements, UInt group_elements, bool *chosen, Groups *previous_groups);
+
+private:
+
+  /**
+   * @brief Returns the indice of the element (row) in the matrix with the greatest communication value sum.
+   * @details This is used to select the first element in a group when invoking generate_groups
+   * @param comm The communication matrix.
+   */
+  static IndexType most_communicating_row(const CommMatrix &comm);
+
+  /**
+   * @brief Register an element to a group.
+   * @details This method has been separated from generate_groups because it was used in different parts of the algorithm.
+   */
+  static inline void register_in_group(const UInt &winner, Group *group, const UInt &pos, bool *chosen, Groups *previous_groups) {
+    chosen[winner] = true;
+    group[pos] = previous_groups[winner];
+  }
 };
 
 #include "eagerMapAlgorithm.ipp"
--- include/algorithms/eagerMap/eagerMapAlgorithm.ipp
+++ include/algorithms/eagerMap/eagerMapAlgorithm.ipp
@@ -1,4 +1,46 @@
-template<typename Load, typename UInt>
-void Algorithms::generateGroups(CommMatrix comm, UInt total_elements, UInt group_elements, bool *chosen) {
+template<typename Load>
+static IndexType Algorithms::UInt most_communicating_row(const CommMatrix &comm) {
+  CommVal max = 0;
+  IndexType chosen = 0;
   
+  for(row : comm.elements) {
+    CommVal sum = 0;
+    for(auto it = row.begin(); it != row.end(); ++it)
+      sum += it->second;
+    if(sum > max) {
+      chosen = it->first;
+      max = sum;
+    }
+  }
+  
+  return chosen;
+}
+
+template<typename Load>
+static Group* Algorithms::generate_groups(CommMatrix comm, UInt total_elements, UInt group_elements, bool *chosen, Groups *previous_groups) {
+  UInt w_max = 0;
+  Group *group = new Group(group_elements);
+  
+  // Caso especial (exceção): Tratar group_elements > total_elements
+  register_in_group(most_communicating_row(comm), group, 0, chosen, previous_groups);
+  
+  for(UInt i = 1; i < group_elements; ++i) {
+    UInt winner = 0;
+    for(UInt j = 0; j < total_elements; ++j)
+      if(!chosen[j]) {
+        UInt w = 0;
+        auto row = comm[j];
+        for(UInt k = 0; k < i; ++k) {
+          auto cell = row.find(group[k]);
+          if(cel != comm[j].end())
+            w += cell->second;
+        }
+        if(w > w_max)
+          w_max = w;
+          winner = j;
+      }
+    register_in_group(winner, group, i, chosen, previous_groups);
+  }
+
+  return group;
 }
